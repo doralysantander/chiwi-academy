eaf5682b2d19273f56176e274668b40ccb16c220
Jymmy Murillo <murillojymmy@gmail.com>
2022-04-21 19:42:59 -0500

    Removing bootstrap classes and editing the necessary ones

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>Meeting Academy</title>
+        
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
