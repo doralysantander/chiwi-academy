ce4fffab854e977ba1cb05fc074dc55d52fac199
Jymmy Murillo <murillojymmy@gmail.com>
2022-04-22 10:25:04 -0500

    Correcting alignment and size of the cards

--- resources/views/components/all-events.blade.php
+++ resources/views/components/all-events.blade.php
@@ -1,8 +1,8 @@
 <div class="own-allEventsSection d-flex flex-column align-items-center p-4">
-    <h4 class="own-h4OutstandingEvents mt-3 text-4xl text-center">
+    <h4 class="mt-3 mb-10 text-4xl text-center text-orange-600 font-bold">
         EVENTOS
     </h4>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 align-items-center justify-content-center">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 align-items-center justify-items-center">
         <x-card/>
         <x-card/>
         <x-card/>
