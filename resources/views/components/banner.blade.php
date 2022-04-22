d5a36677d4a27858f42a1f334e0262720a435419
daniela <moreno.tobar.daniela@gmail.com>
2022-04-22 14:44:41 -0500

    banner done

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,15 +1,16 @@
-{{-- BANNER-1 --}}
+
 
 
 <div class="w-full pt-20">
-    <div class="flex bg-white" style="height:600px;">
+    <div class="flex bg-orange-100" style="height:600px;">
         <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
             <div>
-                <h2 class="text-3xl font-semibold text-neutral-800 md:text-4xl">Aún no hay eventos registrados. <span class="text-orange-600">¡Registra un evento!</span></h2>
-                <div class="flex justify-center lg:justify-start mt-6">
-                    <a class="px-4 py-3 bg-orange-600 text-white text-xs font-semibold rounded-lg" href="{{ route('createEvent.index') }}">Registrar evento</a>
-                    
-                </div>
+                <h2 class="text-2xl sm:text-4xl md:text-3xl lg:text-3xl xl:text-4xl text-center text-neutral-800 font-black leading-7 md:leading-10">Aún no hay eventos registrados. <span class="text-orange-600">¡Registra un evento!</span></h2>
+                <div class="flex justify-center items-center pt-10">
+                    <a class=" bg-orange-600 transition duration-150 ease-in-out lg:text-xl lg:font-bold  rounded-lg text-white px-4 sm:px-10 border py-2 sm:py-4 text-sm"
+                     href="{{ route('createEvent.index') }}">
+                     Registrar evento</a>
+                  </div>
             </div>
         </div>
         <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
--- resources/views/components/banner2.blade.php
+++ resources/views/components/banner2.blade.php
@@ -1,7 +1,7 @@
 
 <dh-component>
 
-    <div class="bg-orange-100">
+    <div class="bg-orange-100 mt-20">
       <div class="container mx-auto flex flex-col items-center py-12 sm:py-24">
         <div class="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
           <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-neutral-800 font-black leading-7 md:leading-10">
@@ -11,8 +11,8 @@
           
         </div>
         <div class="flex justify-center items-center">
-          <button class=" bg-orange-600 transition duration-150 ease-in-out lg:text-xl lg:font-bold  rounded-lg text-white px-4 sm:px-10 border py-2 sm:py-4 text-sm">
-              <a  href="{{ route('createEvent.index') }}">Registrar evento</a></button>
+          <a class=" bg-orange-600 transition duration-150 ease-in-out lg:text-xl lg:font-bold  rounded-lg text-white px-4 sm:px-10 border py-2 sm:py-4 text-sm"
+             href="{{ route('createEvent.index') }}">Registrar evento</a>
         </div>
       </div>
     </div>
